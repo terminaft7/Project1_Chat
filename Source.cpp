a9de7aeeac0a9022f7771985e44e1ba9d89237c3
Artur <89274750+terminaft7@users.noreply.github.com>
2021-08-29 16:00:39 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream> 
+#include "Messages.h"
+#include "User.h"
+using namespace std;
+
+
+int main()
+{
+   char action('a');
+
+    while (action != 'q')
+    {
+
+        cout << "Press 1 to login with your nickname and password " << endl;
+        cout << "Press 2 to create new user " << endl;
+        cout << "Press q to quit" << endl;
+        cin >> action;
+
+        if (action == 'q')
+            break;
+
+
+        switch (action)
+        {
+        case '1':
+            cout << "Enter your nickname" << endl;
+            cin >> nickname;
+            cout << "Enter your password" << endl;
+            cin >> password;
+        //1.здесь отправляем никнейм и пароль на проверку в класс user(nickname, password);
+        //2. если всё ок, подключаем метод отправки сообщений array(messages); как-то так
+            break;
+        case '2':
+            cout << "Enter your nickname" << endl;
+            cin >> nickname;
+            cout << "Enter your password" << endl;
+            cin >> password;
+        //в данном случае мы снова приняли nickname и password, но они будут использованы для запуска конструктора для нового User'a
+            // просьбу ввести пароль второй раз для надёжности реализуем потом
+            break;
+
+
+        return 0;
+    }
+}
---
+++ User.h
@@ -0,0 +1,15 @@
+#pragma once
+class User 
+{
+public : 
+
+protected : 
+    string _nickname;
+    string _password;
+
+    void checkUser(nickname, password);
+    void createUser(nickname, password);
+  
+
+};
+
